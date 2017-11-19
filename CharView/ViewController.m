ca4285f510b4afa2820129f3a68c31f68d6c548d
wuzhilong <wzl87453247@163.com>
2017-11-19 21:25:35 +0800

    Initial Commit

---
+++ CharView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CharView
+//
+//  Created by Mr.wu on 2017/11/19.
+//  Copyright © 2017年 ZR. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
